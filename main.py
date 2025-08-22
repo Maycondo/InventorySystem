99b44b812601a6b31ef38c23c870d05fd0b6926f
maycon <maycondouglasniculau@gmail.com>
2025-08-22 08:56:34 -0300

    refatorar: alterar a estrutura de armazenamento de itens para usar 'Products' em vez de 'items'

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
-
-
-
 class Inventory:
     def __init__(self):
-        self.items = {}
+        self.Products = {}
     
     def add_item(self, item_name, price, quantity):
-        if item_name in self.items:
-            self.items[item_name]['quantity'] += quantity
+        if item_name in self.Products:
+            self.Products[item_name]['quantity'] += quantity
         else:
-            self.items[item_name] = {'price': price, 'quantity': quantity}
+            self.Products[item_name] = {'price': price, 'quantity': quantity}
 
     def get_item(self, item_name):
-        return self.items.get(item_name, None)
-    
+        if item_name not in self.Products:
+            print(f"Item '{item_name}' not found in inventory")
+        else:
+            print(f"Item '{item_name}' found in inventory")
+
     def list_items(self):
-        return self.items
+        if not self.Products:
+            print("No items in inventory")
+        else:
+            print("Items in inventory:")
+            for item, details in self.Products.items():
+                print(f"{item}: Price = {details['price']}, Quantity = {details['quantity']}")
+                print(10 * "-----")
 
     def remove_item(self, item_name, quantity):
-        if item_name in self.items and self.items[item_name]['quantity'] >= quantity:
-            self.items[item_name]['quantity'] -= quantity
-            if self.items[item_name]['quantity'] == 0:
-                del self.items[item_name]
+        if item_name in self.Products and self.Products[item_name]['quantity'] >= quantity:
+            self.Products[item_name]['quantity'] -= quantity
+            if self.Products[item_name]['quantity'] == 0:
+                del self.Products[item_name]
         else:
             raise ValueError("Item not available or insufficient quantity")
     
 
-    
+
 
     
 
