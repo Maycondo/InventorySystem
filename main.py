39bb64d96b79268303c064b4e060b87b65ecb696
maycon <maycondouglasniculau@gmail.com>
2025-10-05 16:10:35 -0300

    adicionar conexão com o banco de dados e criação da tabela de produtos

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
 from InventorySystem import Inventory
+import os
 import pymysql
+
+connection = pymysql.connect(
+    host='localhost',
+    user='root',
+    password='',            
+    database='inventory_db' 
+)
+
+with connection:
+    cursor = connection.cursor()
+    cursor.execute(
+        'CREATE TABLE IF NOT EXISTS products (' 
+            'id INT AUTO_INCREMENT PRIMARY KEY,'
+            'name VARCHAR(255) NOT NULL,'
+            'price DECIMAL(10, 2) NOT NULL,'
+            'quantity INT NOT NULL'
+        ')'
+    )
+    print("Database and table ready")
+    print(50 * "-----")
+    print("Welcome to Inventory System")
+    print(cursor.rowcount, "record(s) affected" )
+    connection.commit()
 
 
 
