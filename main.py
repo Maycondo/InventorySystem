1140a63177af9bb75695d9eab6e91ecab654140a
maycon <maycondouglasniculau@gmail.com>
2025-10-06 09:08:24 -0300

    adicionar tratamento de erros ao criar tabela de produtos no banco de dados

--- main.py
+++ main.py
@@ -9,21 +9,29 @@ connection = pymysql.connect(
     database='inventory_db' 
 )
 
-with connection:
-    cursor = connection.cursor()
-    cursor.execute(
-        'CREATE TABLE IF NOT EXISTS products (' 
-            'id INT AUTO_INCREMENT PRIMARY KEY,'
-            'name VARCHAR(255) NOT NULL,'
-            'price DECIMAL(10, 2) NOT NULL,'
-            'quantity INT NOT NULL'
-        ')'
-    )
-    print("Database and table ready")
-    print(50 * "-----")
-    print("Welcome to Inventory System")
-    print(cursor.rowcount, "record(s) affected" )
-    connection.commit()
+
+try:
+    with connection:
+        cursor = connection.cursor()
+        cursor.execute(
+            'CREATE TABLE IF NOT EXISTS products (' 
+                'id INT AUTO_INCREMENT PRIMARY KEY,'
+                'name VARCHAR(255) NOT NULL,'
+                'price DECIMAL(10, 2) NOT NULL,'
+                'quantity INT NOT NULL'
+            ')'
+        )
+        print("✅ Database and table ready")
+        print(50 * "-----")
+        print("📦 Welcome to Inventory System")
+
+        connection.commit()
+
+except pymysql.MySQLError as e:
+    print(f"❌ Error connecting to database: {e}")
+
+
+
 
 
 
